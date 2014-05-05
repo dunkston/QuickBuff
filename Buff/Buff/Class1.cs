dc573d769c3917facdd606b9fcfb841bc1e2f53b
dunkston <dunkston@gmail.com>
2014-05-04 19:48:21 -0700

    other people can be buffed

--- Buff/Buff/Class1.cs
+++ Buff/Buff/Class1.cs
@@ -25,13 +25,12 @@ namespace Buff
             Commands.ChatCommands.Add(new Command(new List<string>() { "qb.ranged", "qb.all" }, Rabuff, "ranged"));
             Commands.ChatCommands.Add(new Command(new List<string>() { "qb.magic", "qb.all" }, Mabuff, "magic"));
             Commands.ChatCommands.Add(new Command(new List<string>() { "qb.debuff", "qb.all"}, Dbuff, "debuff"));
-            Commands.ChatCommands.Add(new Command("", help, "qb"));
+            Commands.ChatCommands.Add(new Command("", help, "qb", "quickbuff"));
         }
         
         public override Version Version
         {
-            get { return new Version("1.0"); }
-
+            get { return new Version("1.1"); }
         }
 
         public override string Name
@@ -51,11 +50,11 @@ namespace Buff
 
         public static void Rbuff(CommandArgs args)
         {
-            if (args.Parameters.Count > 0)
+            if (args.Parameters.Count > 1)
             {
-                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /regen");
+                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /regen [player]");
             }
-            else if (args.Player != null)
+            else if (args.Parameters.Count == 0 && args.Player != null)
             {
                 args.Player.SetBuff(2, 30000, true);
                 args.Player.SetBuff(5, 30000, true);
@@ -67,14 +66,38 @@ namespace Buff
 
                 args.Player.SendSuccessMessage("You got buffed");
             }
+            else
+            {
+                var plr = TShock.Utils.FindPlayer(args.Parameters[0]);
+                if (plr.Count < 1)
+                {
+                    args.Player.SendErrorMessage("Invalid Player!");
+                }
+                else if (plr.Count > 1)
+                {
+                    TShock.Utils.SendMultipleMatchError(args.Player, plr.Select(p => p.Name));
+                }
+                else
+                {
+                    args.Player.SetBuff(2, 30000, true);
+                    args.Player.SetBuff(5, 30000, true);
+                    args.Player.SetBuff(26, 30000, true);
+                    args.Player.SetBuff(48, 30000, true);
+                    args.Player.SetBuff(58, 30000, true);
+                    args.Player.SetBuff(87, 30000, true);
+                    args.Player.SetBuff(89, 30000, true);
+
+                    args.Player.SendSuccessMessage("You have buffed {0} with the regen buff set", plr[0].Name);
+                }
+            }
         }
         public static void Mbuff(CommandArgs args)
         {
-            if (args.Parameters.Count > 0)
+            if (args.Parameters.Count > 1)
             {
-                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /melee");
+                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /melee [player]");
             }
-            else if (args.Player != null)
+            else if (args.Parameters.Count == 0 && args.Player != null)
             {
                 args.Player.SetBuff(14, 30000, true);
                 args.Player.SetBuff(25, 30000, true);
@@ -84,28 +107,69 @@ namespace Buff
 
                 args.Player.SendSuccessMessage("You got buffed");
             }
+            else
+            {
+                var plr = TShock.Utils.FindPlayer(args.Parameters[0]);
+                if (plr.Count < 1)
+                {
+                    args.Player.SendErrorMessage("Invalid Player!");
+                }
+                else if (plr.Count > 1)
+                {
+                    TShock.Utils.SendMultipleMatchError(args.Player, plr.Select(p => p.Name));
+                }
+                else
+                {
+                    args.Player.SetBuff(14, 30000, true);
+                    args.Player.SetBuff(25, 30000, true);
+                    args.Player.SetBuff(26, 30000, true);
+                    args.Player.SetBuff(62, 30000, true);
+                    args.Player.SetBuff(76, 30000, true);
+
+                    args.Player.SendSuccessMessage("You have buffed {0} with the melee buff set", plr[0].Name);
+                }
+            }
         }
         public static void Rabuff(CommandArgs args)
         {
-            if (args.Parameters.Count > 0)
+            if (args.Parameters.Count > 1)
             {
-                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /ranged");
+                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /ranged [player]");
             }
-            else if (args.Player != null)
+            else if (args.Parameters.Count == 0 && args.Player != null)
             {
                 args.Player.SetBuff(16, 30000, true);
                 args.Player.SetBuff(26, 30000, true);
 
                 args.Player.SendSuccessMessage("You got buffed");
             }
+            else
+            {
+            var plr = TShock.Utils.FindPlayer(args.Parameters[0]);
+            if (plr.Count < 1)
+            {
+                args.Player.SendErrorMessage("Invalid Player!");
+            }
+            else if (plr.Count > 1)
+            {
+                TShock.Utils.SendMultipleMatchError(args.Player, plr.Select(p => p.Name));
+            }
+            else
+            {
+                args.Player.SetBuff(16, 30000, true);
+                args.Player.SetBuff(26, 30000, true);
+
+                args.Player.SendSuccessMessage("You have buffed {0} with the ranged buff set", plr[0].Name);
+            }
+        }
         }
         public static void Mabuff(CommandArgs args)
         {
-            if (args.Parameters.Count > 0)
+            if (args.Parameters.Count > 1)
             {
-                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /magic");
+                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /magic [player]");
             }
-            else if (args.Player != null)
+            else if (args.Parameters.Count == 0 && args.Player != null)
             {
                 args.Player.SetBuff(6, 30000, true);
                 args.Player.SetBuff(7, 30000, true);
@@ -113,14 +177,30 @@ namespace Buff
 
                 args.Player.SendSuccessMessage("You got buffed");
             }
+            else
+            {
+                var plr = TShock.Utils.FindPlayer(args.Parameters[0]);
+                if (plr.Count < 1)
+                {
+                    args.Player.SendErrorMessage("Invalid Player!");
+                }
+                else if (plr.Count > 1)
+                {
+                    TShock.Utils.SendMultipleMatchError(args.Player, plr.Select(p => p.Name));
+                }
+                else
+                {
+                    args.Player.SetBuff(6, 30000, true);
+                    args.Player.SetBuff(7, 30000, true);
+                    args.Player.SetBuff(26, 30000, true);
+
+                    args.Player.SendSuccessMessage("You have buffed {0} with the magic buff set", plr[0].Name);
+                }
+            }
         }
         public static void Dbuff(CommandArgs args)
         {
-            if (args.Parameters.Count < 1)
-            {
-                args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /debuff <player>");
-            }
-            else if (args.Parameters.Count > 1)
+            if (args.Parameters.Count < 1 || args.Parameters.Count > 1)
             {
                 args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /debuff <player>");
             }
@@ -137,26 +217,13 @@ namespace Buff
                 }
                 else
                 {
-                    plr[0].SetBuff(20, 3600, true);
-                    plr[0].SetBuff(21, 3600, true);
-                    plr[0].SetBuff(22, 3600, true);
-                    plr[0].SetBuff(23, 3600, true);
-                    plr[0].SetBuff(24, 3600, true);
-                    plr[0].SetBuff(30, 3600, true);
-                    plr[0].SetBuff(31, 3600, true);
-                    plr[0].SetBuff(32, 3600, true);
-                    plr[0].SetBuff(33, 3600, true);
-                    plr[0].SetBuff(35, 3600, true);
-                    plr[0].SetBuff(36, 3600, true);
-                    plr[0].SetBuff(46, 3600, true);
-                    plr[0].SetBuff(47, 3600, true);
-                    plr[0].SetBuff(67, 3600, true);
-                    plr[0].SetBuff(68, 3600, true);
-                    plr[0].SetBuff(69, 3600, true);
-                    plr[0].SetBuff(70, 3600, true);
-                    plr[0].SetBuff(80, 3600, true);
-                    plr[0].SetBuff(94, 3600, true);
-
+                    plr[0].SetBuff(24, 600, true);
+                    plr[0].SetBuff(30, 600, true);
+                    plr[0].SetBuff(31, 600, true);
+                    plr[0].SetBuff(32, 600, true);
+                    plr[0].SetBuff(33, 600, true);
+                    plr[0].SetBuff(35, 600, true);
+                    plr[0].SetBuff(80, 600, true);
                     args.Player.SendSuccessMessage("You have debuffed {0}", plr[0].Name);
 
                 }
@@ -164,7 +231,7 @@ namespace Buff
         }
         public static void help(CommandArgs args)
         {
-            if (args.Parameters.Count > 2)
+            if (args.Parameters.Count > 2 || args.Parameters[0] != "commands" || args.Parameters[0] != "help")
             {
                 args.Player.SendErrorMessage("Invalid Syntax! Proper syntax: /qb [help] [command]");
             }
@@ -176,9 +243,7 @@ namespace Buff
                     args.Player.SendInfoMessage("Version 1.1");
                     args.Player.SendInfoMessage("For help, type /qb help");
                 }
-                else if (args.Parameters[0] == "help")
-                {
-                    if (args.Parameters.Count == 1)
+                else if (args.Parameters[0] == "commands")
                     {
                         args.Player.SendInfoMessage("Commads:");
                         args.Player.SendInfoMessage("/regen");
@@ -188,45 +253,44 @@ namespace Buff
                         args.Player.SendInfoMessage("/debuff");
                         args.Player.SendInfoMessage("/qb");
                     }
-                    else if (args.Parameters[1] == "regen")
+                else if (args.Parameters[0] == "help")
+                {
+                    
+                    if (args.Parameters[1] == "regen")
                     {
-                        args.Player.SendInfoMessage("Syntax: /regen");
-                        args.Player.SendInfoMessage("Buffs you with");
+                        args.Player.SendInfoMessage("Syntax: /regen [player]");
+                        args.Player.SendInfoMessage("Buffs you or a player with");
                         args.Player.SendInfoMessage(" regeneration, ironskin, well fed, honey, rapid healing,");
                         args.Player.SendInfoMessage("campfire, and heart lamp for 8 minutes");
                     }
                     else if (args.Parameters[1] == "melee")
                     {
-                        args.Player.SendInfoMessage("Syntax: /melee");
-                        args.Player.SendInfoMessage("Buffs you with");
+                        args.Player.SendInfoMessage("Syntax: /melee [player]");
+                        args.Player.SendInfoMessage("Buffs you or a player with");
                         args.Player.SendInfoMessage("Thorns, Tipsy, Well Fed, and Weapon Imbue: Ichor for 8 minutes");
                     }
                     else if (args.Parameters[1] == "ranged")
                     {
-                        args.Player.SendInfoMessage("Syntax: /ranged");
-                        args.Player.SendInfoMessage("Buffs you with");
+                        args.Player.SendInfoMessage("Syntax: /ranged [player]");
+                        args.Player.SendInfoMessage("Buffs you or a player with");
                         args.Player.SendInfoMessage("Archery and Well Fed for 8 minutes");
                     }
                     else if (args.Parameters[1] == "magic")
                     {
-                        args.Player.SendInfoMessage("Syntax: /magic");
-                        args.Player.SendInfoMessage("Buffs you with");
+                        args.Player.SendInfoMessage("Syntax: /magic [player]");
+                        args.Player.SendInfoMessage("Buffs you or a player with");
                         args.Player.SendInfoMessage("Mana Regeneration, Magic Power, and Well Fed for 8 minutes");
                     }
                     else if (args.Parameters[1] == "debuff")
                     {
                         args.Player.SendInfoMessage("Syntax: /debuff <player>");
-                        args.Player.SendInfoMessage("Debuffs a player with");
-                        args.Player.SendInfoMessage("Poisoned, Potion Sickness, Darkness, Cursed,");
                         args.Player.SendInfoMessage("On Fire!, Bleeding, Confused, Slow, Weak,");
-                        args.Player.SendInfoMessage("Silenced, Broken Armor, Cursed Inferno, Chilled,");
-                        args.Player.SendInfoMessage("Frozen, Burning, Ichor, Venom,");
-                        args.Player.SendInfoMessage("Blackout, and Mana Sickness for 10 seconds");
+                        args.Player.SendInfoMessage("Silenced, 6and Blackout for 10 seconds");
                     }
                     else if (args.Parameters[1] == "qb")
                     {
-                        args.Player.SendInfoMessage("Syntax: /qb [help] [command]");
-                        args.Player.SendInfoMessage("Displays plugin info and help");
+                        args.Player.SendInfoMessage("Syntax: /qb [help/commands] [command]");
+                        args.Player.SendInfoMessage("Displays plugin info, commands, and help");
                     }
                     else
                     {
